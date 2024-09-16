ef2369e3e7694f092b3fe7f5e5d526decfca1cd0
Mohan Manjhi <amanjhi1315@gmail.com>
2024-09-16 23:44:27 +0530

    Initial commit

---
+++ work 4.0/Frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './public/views/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
